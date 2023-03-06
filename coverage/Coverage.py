7bd13b867fdf5e0b252fad60d6e5da081e57f180
pengyiteng <pengyiteng@mail.ustc.edu.cn>
2023-03-06 17:54:47 +0800

    coverage output part

--- coverage/Coverage.py
+++ coverage/Coverage.py
@@ -1,5 +1,7 @@
 import torch
 from tqdm import tqdm
+import os
+import pickle
 
 DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
@@ -10,6 +12,8 @@ class Coverage:
         self.model.to(self.device)
         self.layer_size_dict = layer_size_dict
         self.init_variable(hyper, **kwargs)
+
+        self.mia_tag = False
 
     def init_variable(self):
         raise NotImplementedError
@@ -42,8 +46,7 @@ class Coverage:
     def step(self, data):
         cove_dict = self.calculate(data)
         gain = self.gain(cove_dict)
-        if gain < 0:
-            print("Neg")
+
         if gain is not None:
             self.update(cove_dict, gain)
 
@@ -59,3 +62,66 @@ class Coverage:
     def gain(self, cove_dict_new):
         new_rate = self.coverage(cove_dict_new)
         return new_rate - self.current
+
+    '''
+    在研究MIA时，我们更关注单个数据的情况，因此这里给出单个数据的
+    '''
+    # 记录coverage的路径和格式
+    # 文本记录 or 列表数组
+    def mia_set(self, file_path, file_name, save_mode):
+        if save_mode != "txt" and save_mode != "list":
+            print("save_mode should txt or list")
+            return
+
+        self.mia_list = []
+        self.mia_mode = save_mode == "txt"      # txt: True, list: False
+        self.mia_tag = True
+        self.mia_path = os.path.join(file_path, file_name)
+
+    def mia_assess(self, data_loader):
+        if not self.mia_tag:
+            print("please call mia_set first")
+            return
+
+        data_counter = 0
+        for data, *_ in tqdm(data_loader):
+            if isinstance(data, tuple):
+                data = (data[0].to(self.device), data[1].to(self.device))
+            else:
+                data = data.to(self.device)
+            self.mia_step(data)
+
+            data_counter += 1
+            if data_counter == 10000:
+                self.mia_save(data_counter)
+                break
+
+        self.mia_save("end")
+
+    def mia_step(self, data):
+        cove_dict = self.calculate(data)
+        cover = self.coverage(cove_dict)
+
+        if cover is not None:
+            self.mia_list.append(cover)
+
+    def mia_save(self, index):
+        if len(self.mia_list) == 0:
+            return
+
+        if self.mia_mode:
+            # True: txt
+            file_name = self.mia_path + "-" + str(index) + ".txt"
+            with open(file_name, "w") as fp:
+                for each in self.mia_list:
+                    fp.write(str(each) + "\n")
+            print("Writing ", file_name, " ...")
+        else:
+            # False: list
+            file_name = self.mia_path + "-" + str(index) + ".list"
+            with open(file_name, "wb") as fp:
+                pickle.dump(self.mia_list, fp)
+            print("Writing ", file_name, " ...")
+
+        self.mia_list.clear()
+
--- model_cover.py
+++ model_cover.py
@@ -13,17 +13,37 @@ cur_model  = models.LeNet5()
 
 # dataset
 data_name  = "MNIST"
-data_mode  = "test"
+data_mode  = "train"
 loader_tag = True
+
+# load
+load_mode = "s"
+load_path = "pretrained/%s.pt" % (model_name + "_" + load_mode)
 
 # input size
 input_channel = 1
 input_size    = 28
 
+# mia setting
+mia_file_path = "./mia_data"
+mia_mode      = "txt"
+mia_file_name = "%s-%s" % (model_name, data_name)       # 扩展名在写入的时候添加
+
+def load(model, mode):
+    if mode == "s":
+        model = model.to(DEVICE)
+        model.load_state_dict(torch.load(load_path, map_location=DEVICE))
+        return model
+    elif mode == "m":
+        model = torch.load(load_path, map_location=DEVICE)
+        return model
+    else:
+        print("unknown mode")
+
 if __name__ == "__main__":
     #1. Get layer size of model
     input_size = (1, input_channel, input_size, input_size)
-    cur_model = cur_model.to(DEVICE)
+    cur_model = load(cur_model, load_mode)
     random_input = torch.randn(input_size).to(DEVICE)
     layer_size_dict = tool.get_layer_output_sizes(cur_model, random_input)
 
@@ -35,12 +55,14 @@ if __name__ == "__main__":
     
     # 通过测试数据算coverage
     test_loader = data_loader(data_name, data_mode, loader_tag)
-    criterion.assess(test_loader)
+
+    criterion.mia_set(mia_file_path, mia_file_name, mia_mode)
+    criterion.mia_assess(test_loader)
     
     # 如果不是用loader的形式，而是数据流的形式就用这种方式
     # for data in data_stream:
     #     criterion.step(data)
 
     # #3. Result
-    cov = criterion.current
-    print(cov)
+    # cov = criterion.current
+    # print(cov)
